682546fd3ac23e38b4da57ed99d1d88a8ed2f076
Ayush <ayushkushwaha321@gmail.com>
2022-04-02 17:00:30 +0530

    Completed

--- api/helpers/helpers.go
+++ api/helpers/helpers.go
@@ -1,28 +1,27 @@
 package helpers
 
-import(
+import (
 	"os"
 	"strings"
-	"github.com/joho/godotenv"
 )
 
 func EnforceHTTP(url string) string {
-	if url[:4] != "http"{
+	if url[:4] != "http" {
 		return "http://" + url
 	}
 	return url
 }
 
-func RemoveDomainError(url string) bool{
-	if url == os.Getenv("DOMAIN"){
+func RemoveDomainError(url string) bool {
+	if url == os.Getenv("DOMAIN") {
 		return false
 	}
 	newURL := strings.Replace(url, "http://", "", 1)
 	newURL = strings.Replace(newURL, "https://", "", 1)
 	newURL = strings.Replace(newURL, "www.", "", 1)
-	newURL = strings.Replace(newURL, "/")[0]
+	newURL = strings.Split(newURL, "/")[0]
 
-	if newURL == os.Getenv("DOMAIN"){
+	if newURL == os.Getenv("DOMAIN") {
 		return false
 	}
 
--- api/routes/resolve.go
+++ api/routes/resolve.go
@@ -1,26 +1,27 @@
 package routes
 
-import(
+import (
 	"url-shortener/database"
-	"github.com/gp-redis/redis/v8"
+
+	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
 )
 
-func ResolveURL(c *fiber.Ctx) error{
+func ResolveURL(c *fiber.Ctx) error {
 	url := c.Params("url")
 
 	r := database.CreateClient(0)
 	defer r.Close()
 
 	value, err := r.Get(database.Ctx, url).Result()
-	if err != redis.nil{
-		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error":"short not found"})
-		}else if err !=nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error":"cannot connect to database"})
+	if err != redis.Nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "short not found"})
+	} else if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "cannot connect to database"})
 	}
 
 	rInr := database.CreateClient(1)
-	defer rInr.CLose()
+	defer rInr.Close()
 
 	_ = rInr.Incr(database.Ctx, "counter")
 
