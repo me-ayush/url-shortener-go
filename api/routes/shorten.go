fd89ac8b799c106e4f818dfc9a38e4618ae76a2b
Ayush <ayushkushwaha321@gmail.com>
2022-04-02 15:33:21 +0530

    helpers created

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -17,3 +17,27 @@ type response struct{
 	XrateReaminimg int `json:"rate_limit"`
 	XrateLimitRest time.Duration `json:"rate_limit_reset"`
 }
+
+func ShortenURL(c *fiber.Ctx) error{
+	body := new(request)
+	if err:= c.BodyParser(&body); err != nil{
+		c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":"cannot parse JSON"})
+		return
+	}
+
+	// implementing rate limiting
+
+	// check if the input is an actual URL
+	if !govalidator.IsURL(body.URL){
+		c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":"Invaild URL"})
+		return
+	}
+
+	// check for domain error
+	if !helpers.RemoveDomainError(body.URL){
+		return c.Status(fiber.StatusServiceUnavilable).JSON(fiber.Map{"error":"Domain Error"})
+	}
+
+	// enforce https, SSL
+	body.URL = helpers.EnforceHTTP(body.URL)
+}
