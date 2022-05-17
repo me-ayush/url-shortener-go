d58c19c89fce7e36fe99bd80f58c933968778605
Ayush <ayushkushwaha321@gmail.com>
2022-05-17 16:32:39 +0530

    searching and sorted added to uer

--- client/src/components/user/myurl.jsx
+++ client/src/components/user/myurl.jsx
@@ -4,6 +4,7 @@ import Loader from '../loader/loader';
 import Header from '../navbar'
 import './table.scss'
 import swal from 'sweetalert';
+import { handleSearch, sortData } from '../admin/helpers';
 
 
 const Myurl = () => {
@@ -11,6 +12,11 @@ const Myurl = () => {
 	const [isloading, setLoading] = useState(false)
 	const [links, setLinks] = useState([])
 	const [data, setData] = useState(false)
+
+	const [filteredData, setFilteredData] = useState(links);
+	const [searchField, setSearchField] = useState("");
+	const [sortedField, setSortedField] = useState();
+	const [sortBy, setSortBy] = useState(0);
 
 	const token = JSON.parse(localStorage.getItem('token'))
 	const user_id = JSON.parse(localStorage.getItem("id"))
@@ -43,6 +49,7 @@ const Myurl = () => {
 					localStorage.clear();
 				} else {
 					setLinks(data)
+					setFilteredData(data)
 					setData(true)
 				}
 			} catch (err) {
@@ -77,27 +84,74 @@ const Myurl = () => {
 		}
 	}
 
+	const handleChange = (e) => {
+
+		let x = handleSearch(e, searchField, links)
+		if (x != undefined) {
+			setFilteredData(x)
+		}
+	};
+
+	const handelSorting = (field) => {
+		setLoading(true)
+		if (field == sortedField) {
+			setSortBy(sortBy => !sortBy)
+		} else {
+			setSortBy(1)
+		}
+		setSortedField(field)
+		setFilteredData(sortData(field, filteredData, sortBy))
+		setLoading(false)
+	}
+
 
 	return (
 		<>
 			<Header />
 			{isloading && <Loader />}
 			<div id="user-table">
-				<div className="container mt-5 p-0" style={{ "overflow": "auto" }}>
-					<table className="table table-striped table-hover w-100 text-center">
+				<div className="container mt-5 p-0">
+
+					{links &&
+						<form className='mt-2'>
+							<div className='row'>
+								<div className="col-sm-12 col-md-2 mb-2">
+									<select className="form-select shadow-none" onChange={(e) => { setSearchField(e.target.value.toLowerCase()) }} defaultValue={'DEFAULT'} >
+										<option value="DEFAULT" disabled>Select Filter</option>
+										<option value="url">URL</option>
+										<option value="short">Short</option>
+										<option value="clicks">Clicks</option>
+										<option value="expiryat">Expiry</option>
+									</select>
+								</div>
+								<div className="col-sm-12 col-md-9">
+									<input type="search" className='form-control shadow-none' placeholder="Type Something" onChange={(e) => handleChange(e)} />
+								</div>
+							</div>
+						</form>
+					}
+				</div>
+				<div className="container p-0" style={{ "overflow": "auto" }}>
+					<table className="table table-striped table-hover w-100 text-center mt-3">
 						<thead>
 							<tr>
 								<th scope="col">#</th>
-								<th scope="col">URL</th>
+								
+								<th scope="col"><button className='btn text-light btn-none p-0 m-0 shadow-none' onClick={() => handelSorting('url')}>URL</button></th>
+								<th scope="col"><button className='btn text-light btn-none p-0 m-0 shadow-none' onClick={() => handelSorting('short')}>Short</button></th>
+								<th scope="col"><button className='btn text-light btn-none p-0 m-0 shadow-none' onClick={() => handelSorting('clicks')}>Clicks</button></th>
+								<th scope="col"><button className='btn text-light btn-none p-0 m-0 shadow-none' onClick={() => handelSorting('expiry')}>Expiry</button></th>
+
+								{/* <th scope="col">URL</th>
 								<th scope="col">Short</th>
 								<th scope="col">Clicks</th>
-								<th scope="col">Expiry</th>
+								<th scope="col">Expiry</th> */}
 								<th scope="col">Action</th>
 							</tr>
 						</thead>
 						<tbody>
 							{!data && <><tr><td colSpan={7} className='text-center fs-3'>No Links Yet...</td></tr></>}
-							{data && links && links.map((item, i) => (
+							{data && filteredData && filteredData.map((item, i) => (
 								<tr key={i}>
 									<td>{i + 1}</td>
 									{/* <td className="column100 column2" data-column="column2">{item._id}</td> */}
