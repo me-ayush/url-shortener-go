c29ff3b0479bb385a3707a6d1c6a2f45eb796363
Ayush <ayushkushwaha321@gmail.com>
2022-05-17 15:11:42 +0530

    searching and sorting added in all links

--- client/src/components/admin/alllinks.jsx
+++ client/src/components/admin/alllinks.jsx
@@ -12,6 +12,11 @@ const Alllinks = () => {
     const [links, setLinks] = useState(false)
     const [isloading, setLoading] = useState(false)
     const [cookies, setCookies] = useCookies(['user']);
+    const [filteredData, setFilteredData] = useState(links);
+    const [searchField, setSearchField] = useState("");
+    const [rowPerPage, setRowPerPage] = useState(10);
+    const [sortedField, setSortedField] = useState();
+    const [sortBy, setSortBy] = useState(0);
 
     const token = JSON.parse(localStorage.getItem('token'))
     const user_id = JSON.parse(localStorage.getItem("id"))
@@ -41,10 +46,10 @@ const Alllinks = () => {
                     navigate("/")
                 } else {
                     setLinks(data)
+                    setFilteredData(data)
                 }
             } catch (err) {
                 setLoading(false)
-                swal(err, "", "error");
             }
         }
     }
@@ -74,27 +79,90 @@ const Alllinks = () => {
         }
     }
 
+    const handleChange = (e) => {
+        try {
+            let value = e.target.value.toLowerCase();
+            let result = [];
+            result = links.filter((data) => {
+                let x = data[searchField].toLowerCase();
+                return x.search(value) != -1;
+            });
+            setFilteredData(result);
+        } catch { }
+    };
+
+    const sortData = (field) => {
+        setLoading(true)
+        if(field == sortedField){
+            setSortBy(sortBy => !sortBy)
+        }else{
+            setSortBy(1)
+        }
+        setSortedField(field)
+        let x = [...filteredData];
+        if (field !== null) {
+            x.sort((a, b) => {
+                if (a[field] < b[field]) {
+                    return sortBy ? 1 : -1;
+                }
+                if (a[field] > b[field]) {
+                    return sortBy ? -1 : 1;
+                }
+                return 0;
+            });
+        }
+        setFilteredData(x)
+        setLoading(false)
+    }
+
     return (
         <>
             <Header />
             {isloading && <Loader />}
             <div className="container mt-5" style={{ "overflow": "auto" }}>
-                <table className="table  table-striped table-hover w-100 text-center">
+                {links &&
+                    <form className='mt-2'>
+                        <div className='row'>
+                            {/* <div className="input-group mb-3 mt-1"> */}
+                            <div className="col-sm-12 col-md-2 mb-2">
+                                <select className="form-select shadow-none" onChange={(e) => { setSearchField(e.target.value.toLowerCase()) }} defaultValue={'DEFAULT'} >
+                                    <option value="DEFAULT" disabled>Select Filter</option>
+                                    <option value="User">Name</option>
+                                    <option value="url">URL</option>
+                                    <option value="short">Short</option>
+                                    <option value="clicks">Clicks</option>
+                                    <option value="expiryat">Expiry</option>
+                                </select>
+                            </div>
+                            <div className="col-sm-12 col-md-9">
+                                <input type="search" className='form-control shadow-none' placeholder="Type Something" onChange={(e) => handleChange(e)} />
+                            </div>
+                            {/* </div> */}
+                        </div>
+                    </form>
+                }
+                <table className="table  table-striped table-hover w-100 text-center mt-3">
                     <thead>
                         <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Id</th>
-                            <th scope="col">Added By</th>
-                            <th scope="col">URL</th>
-                            <th scope="col">Short</th>
-                            <th scope="col">Clicks</th>
-                            <th scope="col">Expiry</th>
+                            <th scope="col"><button className='btn btn-none p-0 m-0 shadow-none' onClick={() => sortData(null)}>#</button></th>
+                            <th scope="col"><button className='btn btn-none p-0 m-0 shadow-none' onClick={() => sortData('_id')}>ID</button></th>
+                            <th scope="col"><button className='btn btn-none p-0 m-0 shadow-none' onClick={() => sortData('user')}>Added By</button></th>
+                            <th scope="col"><button className='btn btn-none p-0 m-0 shadow-none' onClick={() => sortData('url')}>URL</button></th>
+                            <th scope="col"><button className='btn btn-none p-0 m-0 shadow-none' onClick={() => sortData('short')}>Short</button></th>
+                            <th scope="col"><button className='btn btn-none p-0 m-0 shadow-none' onClick={() => sortData('clicks')}>Clicks</button></th>
+                            <th scope="col"><button className='btn btn-none p-0 m-0 shadow-none' onClick={() => sortData('expiryat')}>Expiry On</button></th>
+
+                            {/* <th scope="col" onClick={() => setSortedField('user')}>Added By</th>
+                            <th scope="col" onClick={() => setSortedField('url')}>URL</th>
+                            <th scope="col" onClick={() => setSortedField('short')}>Short</th>
+                            <th scope="col" onClick={() => setSortedField('clicks')}>Clicks</th>
+                            <th scope="col" onClick={() => setSortedField('expiryat')}>Expiry</th> */}
                             <th scope="col">Action</th>
                         </tr>
                     </thead>
                     <tbody>
-
-                        {links && links.map((item, i) => (
+                        {!links ? <><tr><td colSpan="8" className='text-center fs-3'>No Links</td></tr></> : null}
+                        {filteredData && filteredData.map((item, i) => (
                             <tr key={i}>
                                 <td>{i + 1}</td>
                                 <td>{item._id}</td>
